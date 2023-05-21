401594ee5cffca4a2f0695973b33833b51ef60a7
Samuel Welday <wediwelday23@gmail.com>
2023-05-21 12:20:44 -0500

    Adding annotations & getter setter

--- src/main/java/finalProject/domain/Order.java
+++ src/main/java/finalProject/domain/Order.java
@@ -1,11 +1,34 @@
 package finalProject.domain;
 
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+@Entity
+@Data
+@NoArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue
     private int id;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Customer customer;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Address address;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<OrderLine> orderLineList;
 
+    public Order(Customer customer, Address address) {
+        this.customer = customer;
+        this.address = address;
+    }
+
+    public List<OrderLine> getOrderLineList() {
+        return orderLineList;
+    }
+
+    public void setOrderLineList(OrderLine order) {
+        this.orderLineList.add(order);
+    }
 }
