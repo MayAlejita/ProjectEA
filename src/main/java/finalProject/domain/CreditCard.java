7e621667388ad3b998368cd29a0628b94bcc7572
Mussie <mussieayub@gmail.com>
2023-05-21 12:12:23 -0500

    created annotations and setters

--- src/main/java/finalProject/domain/CreditCard.java
+++ src/main/java/finalProject/domain/CreditCard.java
@@ -1,10 +1,26 @@
 package finalProject.domain;
 
-import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+@Entity
+@Data
+@NoArgsConstructor
 public class CreditCard {
+    @Id
+    @GeneratedValue
     private int id;
     private int numberCard;
     private String expirationDate;
     private String securityCode;
+
+    public CreditCard(int numberCard, String expirationDate, String securityCode) {
+        this.numberCard = numberCard;
+        this.expirationDate = expirationDate;
+        this.securityCode = securityCode;
+    }
 }
