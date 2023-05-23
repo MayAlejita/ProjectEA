75efaf33404903523450a04c217f8c0f114403f0
Omar-H-Assaf <omar.assaf@hearst.com>
2023-05-22 22:37:09 -0500

    Add items methods

--- src/main/java/finalProject/controller/ItemController.java
+++ src/main/java/finalProject/controller/ItemController.java
@@ -1,12 +1,14 @@
 package finalProject.controller;
 import finalProject.domain.Item;
 import finalProject.Service.ItemService;
+import finalProject.dto.ItemDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/items")
@@ -15,7 +17,25 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
-    @GetMapping
-    public Page<Item> findAll(Pageable pageable) { return itemService.getAllItems(pageable); }
+    @GetMapping("/")
+    public Page<Item> findAllItems(Pageable pageable) { return itemService.getAllItems(pageable); }
+
+    @GetMapping("/{id}")
+    public Item getItemById(@PathVariable int id) {
+        return itemService.findItemById(id);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> addItem(ItemDTO itemDTO){
+        ItemDTO item =  itemService.addItem(itemDTO);
+        return new ResponseEntity<>(item, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteItem(@PathVariable int id) {
+        itemService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
 }
