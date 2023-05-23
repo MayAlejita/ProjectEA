379cff042209de21af34f3f57560ed70f17f7f9b
Samuel-Welday <wediwelday23@gmail.com>
2023-05-23 13:09:13 -0500

    customer servce impl update

--- src/main/java/finalProject/dto/OrderDTO.java
+++ src/main/java/finalProject/dto/OrderDTO.java
@@ -1,4 +1,16 @@
 package finalProject.dto;
 
+import finalProject.domain.OrderLine;
+import finalProject.domain.Status;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class OrderDTO {
+
+    private Status status;
+
+    private List<OrderLine> orderLineList;
+
 }
